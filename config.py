450b634cec1664789a6e949d332c86f5a93abfbc
Afryk <149395887+Afryk@users.noreply.github.com>
2023-11-16 08:20:44 +0200

    config.py

--- config.py
+++ config.py
@@ -5,22 +5,22 @@ from collections import OrderedDict
 
 
                     #Null reiksmiu trinimas
-# csv_file_path = 'scraped_data.csv'
-# output_file_path = 'scraped_data.csv'
-#
-# with open(csv_file_path, 'r') as input_file:
-#     reader = csv.reader(input_file)
-#     header = next(reader)
-#     indices = {column: index for index, column in enumerate(header)}
-#     rows_without_null = [header]
-#
-#
-#     for row in reader:
-#         if not any(cell is None or cell == '' for cell in row):
-#             rows_without_null.append(row)
-# with open(output_file_path, 'w', newline='') as output_file:
-#     writer = csv.writer(output_file)
-#     writer.writerows(rows_without_null)
+csv_file_path = 'scraped_data.csv'
+output_file_path = 'scraped_data.csv'
+
+with open(csv_file_path, 'r') as input_file:
+    reader = csv.reader(input_file)
+    header = next(reader)
+    indices = {column: index for index, column in enumerate(header)}
+    rows_without_null = [header]
+
+
+    for row in reader:
+        if not any(cell is None or cell == '' for cell in row):
+            rows_without_null.append(row)
+with open(output_file_path, 'w', newline='') as output_file:
+    writer = csv.writer(output_file)
+    writer.writerows(rows_without_null)
 
 
                     #Stulpeliu formotavimas
@@ -43,15 +43,15 @@ print("openaq.csv")
 
 
                     #Stulpeliu pavadinimo keitimas
-# df = pd.read_csv('scraped_data.csv')
-# df.rename(columns={'0': 'ID'}, inplace=True)
-# df.rename(columns={'1': 'Country'}, inplace=True)
-# df.rename(columns={'2': 'Pollutant'}, inplace=True)
-# df.rename(columns={'3': 'Value'}, inplace=True)
-# df.rename(columns={'4': 'Unit'}, inplace=True)
-# df.rename(columns={'5': 'Last Update'}, inplace=True)
-# df.rename(columns={'6': 'Source Name'}, inplace=True)
-# df.to_csv("scraped_data.csv", index=False)
-# print(df)
+df = pd.read_csv('scraped_data.csv')
+df.rename(columns={'0': 'ID'}, inplace=True)
+df.rename(columns={'1': 'Country'}, inplace=True)
+df.rename(columns={'2': 'Pollutant'}, inplace=True)
+df.rename(columns={'3': 'Value'}, inplace=True)
+df.rename(columns={'4': 'Unit'}, inplace=True)
+df.rename(columns={'5': 'Last Update'}, inplace=True)
+df.rename(columns={'6': 'Source Name'}, inplace=True)
+df.to_csv("scraped_data.csv", index=False)
+print(df)
 
 
